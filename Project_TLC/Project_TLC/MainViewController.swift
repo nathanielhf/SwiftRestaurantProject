e7a0c7e1de7bc50390ab1eb3344d6dd2f9f60b71
Sidebail <tfg.devteam@gmail.com>
2019-07-17 14:13:51 -0400

    Really bad try onto wireframing within the XCode

---
+++ Project_TLC/Project_TLC/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_TLC
+//
+//  Created by Vladimir Vatcurin on 2019-07-17.
+//  Copyright © 2019 TeamRestaurant. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
